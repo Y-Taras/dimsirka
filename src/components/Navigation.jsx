89ad9ab0e9b9ff3a7002cd9cac2173745e330f66
Taras <ytaras@gmail.com>
2017-10-05 23:47:43 +0300

    added navigation bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,74 +3,98 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
+import FaBars from 'react-icons/lib/fa/bars';
 import AccordionSection from './NavigationAccordion';
 
-const Navigation = ({ urlPrefix }: { urlPrefix: string }) => (
-  <div className="header__bottom">
-    <div className="header__logo">
-      <Link to={`/${urlPrefix}`}>
-        <img src="/dist/images/logo.png" alt="dog logo" />
-      </Link>
-    </div>
-    <div className="nav-container">
-      <nav className="nav nav-left">
-        <AccordionSection title={'ourAnimals'}>
-          <NavLink className="dropdown__link" to={`/${urlPrefix}takeDog`}>
-            <FormattedMessage id="header.ourAnimals.takeDog" />
+type Props = {
+  urlPrefix: string,
+}
+
+class Navigation extends React.Component {
+  state = {
+      open: false
+  }
+
+  handleNavClick = (e) => {
+    e.preventDefault();
+    this.setState({
+      open: !this.state.open
+    });
+  }
+
+  props: Props;
+
+  render() {
+    const navToggle = this.state.open ? 'nav-open' : 'nav-closed';
+    return (
+      <div className="header__bottom">
+        <div className="header__logo">
+          <Link to={`/${this.props.urlPrefix}`}>
+            <img className={'header__logo-image'} src="/dist/images/logo.png" alt="site logo" />
+          </Link>
+          <button type={'button'} onClick={this.handleNavClick} className={'nav__bar-button'}><FaBars size={'40'}/></button>
+        </div>
+        <div className={`nav-container ${navToggle}`}>
+          <nav className="nav nav-left">
+            <AccordionSection title={'ourAnimals'}>
+              <NavLink className="dropdown__link" to={`/${this.props.urlPrefix}takeDog`}>
+                <FormattedMessage id="header.ourAnimals.takeDog" />
+              </NavLink>
+              <NavLink className="dropdown__link" to={`/${this.props.urlPrefix}takeCat`}>
+                <FormattedMessage id="header.ourAnimals.takeCat" />
+              </NavLink>
+              <NavLink className="dropdown__link" to={`/${this.props.urlPrefix}onlineCare`}>
+                <FormattedMessage id="header.ourAnimals.careOnline" />
+              </NavLink>
+            </AccordionSection>
+            <NavLink className="nav__link" to={`/${this.props.urlPrefix}projects`}>
+              <FormattedMessage id="header.projects" />
+            </NavLink>
+            <AccordionSection title={'search'}>
+              <NavLink className="dropdown__link" to={`/${this.props.urlPrefix}#`}>
+                <FormattedMessage id="header.search.lost" />
+              </NavLink>
+              <NavLink className="dropdown__link" to={`/${this.props.urlPrefix}#`}>
+                <FormattedMessage id="header.search.found" />
+              </NavLink>
+            </AccordionSection>
+          </nav>
+        </div>
+        <div className={`nav-container ${navToggle}`}>
+          <nav className="nav nav-right">
+            <AccordionSection title={'news'}>
+              <NavLink className="dropdown__link" to={`/${this.props.urlPrefix}news/our`}>
+                <FormattedMessage id="header.news.ourNews" />
+              </NavLink>
+              <NavLink className="dropdown__link" to={`/${this.props.urlPrefix}news/actions`}>
+                <FormattedMessage id="header.news.actions" />
+              </NavLink>
+              <NavLink className="dropdown__link" to={`/${this.props.urlPrefix}news/info`}>
+                <FormattedMessage id="header.news.useful" />
+              </NavLink>
+            </AccordionSection>
+            <NavLink className="nav__link" to={`/${this.props.urlPrefix}shop`}>
+              <FormattedMessage id="header.shop" />
+            </NavLink>
+            <AccordionSection title={'aboutUs'}>
+              <NavLink className="dropdown__link" to={`/${this.props.urlPrefix}name`}>
+                <FormattedMessage id="header.aboutUs.name" />
+              </NavLink>
+              <NavLink className="dropdown__link" to={`/${this.props.urlPrefix}team`}>
+                <FormattedMessage id="header.aboutUs.team" />
+              </NavLink>
+              <NavLink className="dropdown__link" to={`/${this.props.urlPrefix}partners`}>
+                <FormattedMessage id="header.aboutUs.partners" />
+              </NavLink>
+            </AccordionSection>
+          </nav>
+          <NavLink className="nav__link nav__button" to={`/${this.props.urlPrefix}support`}>
+            <FormattedMessage id="header.support" />
           </NavLink>
-          <NavLink className="dropdown__link" to={`/${urlPrefix}takeCat`}>
-            <FormattedMessage id="header.ourAnimals.takeCat" />
-          </NavLink>
-          <NavLink className="dropdown__link" to={`/${urlPrefix}onlineCare`}>
-            <FormattedMessage id="header.ourAnimals.careOnline" />
-          </NavLink>
-        </AccordionSection>
-        <NavLink className="nav__link" to={`/${urlPrefix}projects`}>
-          <FormattedMessage id="header.projects" />
-        </NavLink>
-        <AccordionSection title={'search'}>
-          <NavLink className="dropdown__link" to={`/${urlPrefix}#`}>
-            <FormattedMessage id="header.search.lost" />
-          </NavLink>
-          <NavLink className="dropdown__link" to={`/${urlPrefix}#`}>
-            <FormattedMessage id="header.search.found" />
-          </NavLink>
-        </AccordionSection>
-      </nav>
-    </div>
-    <div className="nav-container">
-      <nav className="nav nav-right">
-        <AccordionSection title={'news'}>
-            <NavLink className="dropdown__link" to={`/${urlPrefix}news/our`}>
-              <FormattedMessage id="header.news.ourNews" />
-            </NavLink>
-            <NavLink className="dropdown__link" to={`/${urlPrefix}news/actions`}>
-              <FormattedMessage id="header.news.actions" />
-            </NavLink>
-            <NavLink className="dropdown__link" to={`/${urlPrefix}news/info`}>
-              <FormattedMessage id="header.news.useful" />
-            </NavLink>
-        </AccordionSection>
-        <NavLink className="nav__link" to={`/${urlPrefix}shop`}>
-          <FormattedMessage id="header.shop" />
-        </NavLink>
-        <AccordionSection title={'aboutUs'}>
-            <NavLink className="dropdown__link" to={`/${urlPrefix}name`}>
-              <FormattedMessage id="header.aboutUs.name" />
-            </NavLink>
-            <NavLink className="dropdown__link" to={`/${urlPrefix}team`}>
-              <FormattedMessage id="header.aboutUs.team" />
-            </NavLink>
-            <NavLink className="dropdown__link" to={`/${urlPrefix}partners`}>
-              <FormattedMessage id="header.aboutUs.partners" />
-            </NavLink>
-        </AccordionSection>
-      </nav>
-      <NavLink className="nav__link nav__button" to={`/${urlPrefix}support`}>
-        <FormattedMessage id="header.support" />
-      </NavLink>
-    </div>
-  </div>
-);
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Navigation;
